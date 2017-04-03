59cc22db91343fcb4a7296c80482fe20435838f2
Naeem Hasan <naeemh1396@gmail.com>
2017-04-03 16:37:22 +0600

    Delete simple.py

--- examples/simple.py
+++
@@ -1,7 +0,0 @@
-from elektra import ElektraSearch
-
-search = ElektraSearch()
-search.add_words("python", "google", "harry", "potter", "accio")
-search.make_puzzle()
-search.render_photo()
-search.save_puzzle("simple.jpg")
